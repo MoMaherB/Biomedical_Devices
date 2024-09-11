0d9581c24b2368dffc0ad1e6d5a33b290e3e540c
MoMaherB <maher1525@gmail.com>
2024-09-11 21:27:13 +0300

    landing page

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ class User(db.Model):
 
 
 @app.route('/')
-@app.route('/home')
-def home():
-	return render_template('home.html')
+@app.route('/index')
+def index():
+	return render_template('index.html')
 
 
 @app.route('/login', methods=["GET", "POST"])
